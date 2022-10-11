43f131c8a50dcfa5a49bb47761d17765a5b625a7
Lyuben Sirakov <lyuben.sirakov@Lyubens-MacBook-Pro.local>
2022-10-11 13:39:14 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,64 @@
-import logo from './logo.svg';
+import pepe from './images/pepe.png'
+import dolan from './images/dolan.png'
+import gooby from './images/gooby.png'
+import trading from './images/trading.png'
+import hasbulla from './images/has.png'
+import algo from './algo';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [message, setMessage] = useState('')
+
+  const handleChange = (e) => setMessage(e.target.value)
+
+  let newAlgoSenctence = algo(message)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='img-container'>
+          <img src={pepe} className="App-logo" alt="logo" />
+          <img src={dolan} className="App-logo" alt="logo" />
+          <img src={gooby} className="App-logo" alt="logo" />
+          <img src={trading} className="App-logo" alt="logo" />
+        </div>
+        <h1>
+          Enter something to algo
+        </h1>
+        <div className='input-div'>
+          <img src={hasbulla} className="hasbulla" alt="hasbulla" />
+          <input
+            type="text"
+            id="message"
+            name="message"
+            onChange={handleChange}
+            value={message}
+          />
+        </div>
+        <div className='ouput-div'>
+          {newAlgoSenctence
+            ? (
+              <>
+                <h3 className='output'>{newAlgoSenctence}</h3>
+                <button
+                  className='algo-btn'
+                  onClick={() => { navigator.clipboard.writeText(newAlgoSenctence) }}
+                >
+                  Copy
+                </button>
+              </>
+            )
+            : (
+              <>
+                <h3 className='output'>Размени буквите</h3>
+                <button
+                  className='btn-invisible '
+                >
+                </button>
+              </>
+            )}
+        </div>
       </header>
     </div>
   );
